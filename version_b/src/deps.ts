ceebfe1fe186028f170c40db253b67eaccd6cd1f
24online24 <chiuzbaianrares20@gmail.com>
2023-05-18 19:05:37 +0300

    Version B started

---
+++ version_b/src/deps.ts
@@ -0,0 +1,17 @@
+export {
+  Application,
+  helpers,
+  Router,
+} from "https://deno.land/x/oak@v12.4.0/mod.ts";
+export type {
+  Context,
+  RouterContext,
+} from "https://deno.land/x/oak@v12.4.0/mod.ts";
+export { config as dotenvConfig } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
+export { z } from "https://deno.land/x/zod@v3.16.1/mod.ts";
+export {
+  Bson,
+  Database,
+  MongoClient,
+  ObjectId,
+} from "https://deno.land/x/mongo@v0.30.1/mod.ts";
---
+++ version_b/src/server.ts
@@ -0,0 +1,32 @@
+import { Application, Router } from "./deps.ts";
+import type { RouterContext } from "./deps.ts";
+import config from "./config/default.ts";
+
+const app = new Application();
+
+const router = new Router();
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+router.get<string>("/api/healthchecker", (ctx: RouterContext<string>) => {
+  ctx.response.body = {
+    status: "success",
+    message: "Welcome to Deno and MongoDB",
+  };
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener("listen", ({ port, secure }) => {
+  console.log(
+    `🚀 Server started on ${secure ? "https://" : "http://"}localhost:${port}`,
+  );
+});
+
+const port = config.port;
+app.listen({ port });
