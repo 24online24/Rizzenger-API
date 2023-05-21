46fe7105a0490e7f44d4d33817480058b7a06fae
24online24 <chiuzbaianrares20@gmail.com>
2023-05-21 23:08:10 +0300

    Fixed typo for DB connecton

--- version_b/src/config/default.ts
+++ version_b/src/config/default.ts
@@ -7,8 +7,8 @@ const config: {
   dbName: string;
 } = {
   port: parseInt(Deno.env.get("PORT") as unknown as string),
-  dbUri: Deno.env.get("MONGO_URI") as unknown as string,
-  dbName: Deno.env.get("MONGO_DATABASE_NAME") as unknown as string,
+  dbUri: Deno.env.get("MONGODB_URI") as unknown as string,
+  dbName: Deno.env.get("MONGODB_DATABASE_NAME") as unknown as string,
 };
 
 export default config;
